63a4e11419221f27ad3b8c87e62aac6d29e8d3e3
Simon <jim48@gmx.de>
2023-11-24 18:05:18 +0100

    idk just PUSH harder

--- 03 Lichterfeld (Vorlage schwer)/MAIN.java
+++ 03 Lichterfeld (Vorlage schwer)/MAIN.java
@@ -1,24 +1,18 @@
 public class MAIN
 {
     public static void main(String[] args) {
-        //LICHTERFELD lichterfeld = new LICHTERFELD();
-        //lichterfeld.AlleLampenAusschalten();
-        //lichterfeld.AbwechselndEinschalten();
-
-
+//        LICHTERFELD lichterfeld = new LICHTERFELD();
+//        lichterfeld.AlleLampenAusschalten();
+//        lichterfeld.AbwechselndEinschalten();
                 GRAPH_MATRIX graph_matrix = new GRAPH_MATRIX(10);
-                graph_matrix.KnotenEinfuegen("A");
-                graph_matrix.KnotenEinfuegen("B");
-                graph_matrix.Ausgeben();
-                graph_matrix.KanteEinfuegen("A","B",50);
-                graph_matrix.KanteEinfuegen("A","A",50);
-                graph_matrix.Ausgeben();
-                graph_matrix.KnotenBezeichnerGeben(1);
-                graph_matrix.KanteGewichtGeben("A", "B");
-
-
-
+                graph_matrix.KnotenEinfuegen("A", 5,5);
+//                graph_matrix.KnotenEinfuegen("B");
+//                graph_matrix.Ausgeben();
+//                graph_matrix.KanteEinfuegen("A","B",50);
+//                graph_matrix.KanteEinfuegen("A","A",50);
+//                graph_matrix.Ausgeben();
+//                graph_matrix.KnotenBezeichnerGeben(1);
+//                graph_matrix.KanteGewichtGeben("A", "B");
 
     }
-
 }
